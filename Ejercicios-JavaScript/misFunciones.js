90e8ceeb3ebb9cf32d4ebb66e8d9ad007ed14c70
nicolas bergami <nicolasbergami2013@gmail.com>
2020-05-25 19:12:33 -0300

    conversor de funciones añadido

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -9,3 +9,30 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function CambiarUnidades(id,valor){
+    if(isNaN(value)){
+        alert("se ingreso un valor invalido");
+        document.LasUnidades.unid_metro.value=" ";
+    }else if (id=="metro"){
+        document.LasUnidades.unid_pulgada.value=39.37*valor;
+        document.LasUnidades.unid_pie.value=3.28084*valor;
+        document.LasUnidades.unid_yarda.value=1.0936*valor;
+    }
+    else if (id=="pulgada"){
+        document.LasUnidades.unid_metro.value=0.0254*valor;
+        document.LasUnidades.unid_pie.value=0.08333*valor;
+        document.LasUnidades.unid_yarda.value=0.02777*valor;
+    }
+    else if (id=="yarda"){
+        document.LasUnidades.unid_metro.value=0.9144*valor;
+        document.LasUnidades.unid_pie.value=3*valor;
+        document.LasUnidades.unid_pulgada.value=36*valor;
+
+    }
+    else if (id=="pie"){
+        document.LasUnidades.unid_metro.value=0.3048*valor;
+        document.LasUnidades.unid_yarda.value=0.333333*valor;
+        document.LasUnidades.unid_pulgada.value=11.999988*valor;
+
+    }
+}
